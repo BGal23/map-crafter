c5fda1f3147b2032f252fbeaec0af1369b4aa9a9
Bartek <bartek.gal23@gmail.com>
2024-09-24 10:00:21 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,62 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-// Styl dla kontenera mapy (ustawiasz wysokość, ponieważ domyślnie element może być niewidoczny)
 const mapContainerStyle = {
   height: "100vh",
   width: "100vw",
 };
 
-// Ustawienia początkowe mapy
-const center = {
-  lat: 52.2297, // Szerokość geograficzna Warszawy
-  lng: 21.0122, // Długość geograficzna Warszawy
+const defaultCenter = {
+  lat: 52.2297,
+  lng: 21.0122,
 };
 
-const App: React.FC = () => {
+const App = () => {
+  const [currentLocation, setCurrentLocation] = useState(defaultCenter);
+
+  useEffect(() => {
+    const watchId = checkMyLocation();
+
+    // Czyszczenie watchPosition po odmontowaniu komponentu
+    return () => {
+      if (navigator.geolocation && watchId) {
+        navigator.geolocation.clearWatch(watchId);
+      }
+    };
+  }, []);
+
+  const checkMyLocation = () => {
+    if (navigator.geolocation) {
+      // Użycie watchPosition do ciągłego śledzenia lokalizacji
+      const watchId = navigator.geolocation.watchPosition((position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+      return watchId; // Zwracamy watchId, aby można było go wyczyścić później
+    } else {
+      console.log("error");
+
+      return null;
+    }
+  };
+
   return (
-    // Komponent LoadScript zapewnia załadowanie skryptu Google Maps API
-    <LoadScript googleMapsApiKey="AIzaSyBL7C2LRxclklMp9It5Ghb0vEYYO56L8AU">
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={10} // Poziom zbliżenia
-      >
-        {/* Dodanie markera */}
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <>
+      {currentLocation && (
+        <LoadScript googleMapsApiKey="AIzaSyBL7C2LRxclklMp9It5Ghb0vEYYO56L8AU">
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={currentLocation}
+            zoom={12} // Zmniejszyłem zoom dla lepszej widoczności na niższym poziomie
+          >
+            {/* Dodanie markera */}
+            <Marker position={currentLocation} />
+          </GoogleMap>
+        </LoadScript>
+      )}
+    </>
   );
 };
 
